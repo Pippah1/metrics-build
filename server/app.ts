f9db4cdfb06fbe3704760bc6c4ad9028a66acd80
andrikuusk <andrikuusk@gmail.com>
2025-10-21 01:02:10 +0300

    dockerfile runs build now

--- server/app.ts
+++ server/app.ts
@@ -26,6 +26,6 @@ app.get('/metrics', cors(), (req, res) => {
   }, 1000);
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
