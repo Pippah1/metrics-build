11991772fda0b2824417f21be296ad043d4cab8c
andrikuusk <andrikuusk@gmail.com>
2025-12-20 12:42:16 +0200

    load-test

---
+++ k6/loadtest.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 5,
+  duration: '30s',
+  thresholds: {
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(95)<500'],
+  },
+};
+
+export default function () {
+  const res = http.get('http://http://192.168.86.41/');
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+  sleep(1);
+}
